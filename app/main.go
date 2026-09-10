e01b8ca8dbf92d3787473ed2ede37fb04c72f2d9
Ricky Roy Nardson <rickyroynardson@gmail.com>
2026-09-10 15:37:29 +0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -4,39 +4,37 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"slices"
 	"strings"
 )
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
+	builtins := []string{"echo", "exit", "type"}
+
 	for {
 		fmt.Print("$ ")
 
-		i, err := reader.ReadString('\n')
+		cmd, err := reader.ReadString('\n')
 		if err != nil {
 			fmt.Fprintln(os.Stderr, "error reading input:", err)
 			os.Exit(1)
 		}
 
-		i = strings.TrimSpace(i)
-		inputs := strings.Split(i, " ")
-		cmd := inputs[0]
-		args := inputs[1:]
-		builtins := map[string]struct{}{
-			"echo": {},
-			"exit": {},
-			"type": {},
-		}
-		switch cmd {
+		cmd = strings.TrimSpace(cmd)
+		tokens := strings.Split(cmd, " ")
+
+		switch tokens[0] {
 		case "exit":
 			os.Exit(0)
 		case "echo":
-			fmt.Println(strings.Join(args, " "))
+			fmt.Println(strings.Join(tokens[1:], " "))
 		case "type":
-			if _, ok := builtins[args[0]]; ok {
-				fmt.Printf("%s is a shell builtin\n", args[0])
+			// panic: need to handle index out of range if args is empty
+			if slices.Contains(builtins, tokens[1]) {
+				fmt.Printf("%s is a shell builtin\n", tokens[1])
 			} else {
-				fmt.Printf("%s: not found\n", args[0])
+				fmt.Printf("%s: not found\n", tokens[1])
 			}
 		default:
 			fmt.Printf("%s: command not found\n", cmd)
